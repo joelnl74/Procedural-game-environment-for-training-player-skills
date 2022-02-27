3c0612345f7cd69fba382835c276f0dc7d1a96ab
Joey <joelnl74@gmail.com>
2022-02-27 13:00:48 +0100

    fix a few parameters

--- SMB-clone-master/Assets/Scripts/Mario/Mario.cs
+++ SMB-clone-master/Assets/Scripts/Mario/Mario.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Mario : MonoBehaviour {
 	private LevelManager t_LevelManager;
@@ -24,9 +25,9 @@ public class Mario : MonoBehaviour {
 	private float minWalkSpeedX = .28f;
 	private float walkAccelerationX = .14f;
 	private float runAccelerationX = .21f;
-	private float releaseDecelerationX = .25f; // original: .19f;
-	private float skidDecelerationX = .5f; // .38f;
-	private float skidTurnaroundSpeedX = 3.5f; // 2.11;
+	private float releaseDecelerationX = .19f;
+	private float skidDecelerationX = .38f;
+	private float skidTurnaroundSpeedX = 2.11f;
 	private float maxWalkSpeedX = 5.86f;
 	private float maxRunSpeedX = 9.61f;
 
